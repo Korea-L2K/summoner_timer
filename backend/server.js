06e54a0ea4248ce741d50c098728ac3fd5d003b5
JeffreyChang12 <72239159+JeffreyChang12@users.noreply.github.com>
2025-09-22 09:06:49 -0400

    edits

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,31 @@
-import express from "express";
-import { createServer } from "http";
-import { Server } from "socket.io";
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-  cors: { origin: "https://summoner-timer.vercel.app" }
+  cors: { origin: 'https://summoner-timer.vercel.app' }
 });
 
 const rooms = {};
 
-io.on("connection", socket => {
-  console.log("user connected");
+io.on('connection', socket => {
+  console.log('user connected');
 
-  socket.on("join", room => {
+  socket.on('join', room => {
     socket.join(room);
-    if (rooms[room]?.start) socket.emit("start", rooms[room].start);
+    if (rooms[room]?.start) socket.emit('start', rooms[room].start);
   });
 
-  socket.on("startTimer", room => {
+  socket.on('startTimer', room => {
     const start = Date.now();
     rooms[room] = { start };
-    io.to(room).emit("start", start);
+    io.to(room).emit('start', start);
   });
 
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
   });
 });
 
